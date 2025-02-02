247ac32258f3a3ab695deab0a56a547dd28c3d1d
Amauri1327 <amauri1816@hotmail.com>
2025-02-01 21:52:50 -0300

    criacao UserRecordDto

--- user/src/main/java/com/ms/user/controllers/UserController.java
+++ user/src/main/java/com/ms/user/controllers/UserController.java
@@ -1,8 +1,10 @@
 package com.ms.user.controllers;
 
 
+import com.ms.user.dtos.UserRecordDto;
 import com.ms.user.models.UserModel;
 import jakarta.validation.Valid;
+import org.springframework.beans.BeanUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,6 +16,9 @@ public class UserController {
 
     @PostMapping("/users")
     public ResponseEntity<UserModel> saveUser(@RequestParam @Valid UserRecordDto userRecordDto){
+
+        var userModel = new UserModel();
+        BeanUtils.copyProperties(userRecordDto, userModel);
 
         return ResponseEntity.status(HttpStatus.CREATED).body();
     }
